ddcd43a5ab01bad987f5a44a8e65ad97bc2908c7
HinesDavidAlexander <alex.hines@adhtech.com>
2024-02-08 17:48:50 -0800

    Weather and Location functionality implemented. Notes for further improvement added to README

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This project is a one-day experiment aimed at creating a simple version of a hom
 
 ## Features
 
-- **Voice Command Recognition**: Accept basic voice commands to perform tasks like turning on/off lights, playing music, or checking the weather.
+- **Voice Command Recognition**: Accept basic voice commands to perform tasks like turning on/off lights or checking the weather.
 - **Device Integration**: Basic integration with simulated home devices (e.g., lights, thermostat).
 - **Weather Updates**: Provide current weather information upon request.
 
@@ -18,18 +18,30 @@ Given the time constraint of a single day, the following limitations apply:
 - Simplified voice recognition: May not accurately interpret all commands due to the use of basic voice recognition techniques.
 - Basic functionality: Features will be rudimentary, focusing on demonstrating the concept rather than providing a polished user experience.
 
+## Future Possible Improvements
+
+The following are a list of potential future improvements that could be made based on the current state of the application.
+
+- Decorators for Action Consistency:
+    - Due to the nature of how Actions are handled, requiring specific return types, ensuring consistency is good. Create a decorator that takes the Action output and converts it to str if not already str.
+    - This would open up the option to further improve using this decorator, options like auto-filling the actions dict and similar functionality come to mind.
+- Simulated Voice for readouts
+- NLP for similarity to 'run command' action phrase, recognition isn't always accurate and may need a few tries sometimes
+- NLP for simmilarity to command options from the dict, same reason as prior note
+- More advanced GUI, current GUI implementation targets function over style, and therefore is quite rudamentary.
+
 ## Technologies Used
 
 - **Programming Language**: Python 3.x (for its extensive support for quick prototyping and available libraries for device control and voice recognition).
-- **Libraries**: 
+- **Libraries**: See requirements.txt
 
 
 ## Setup Instructions
 
 1. **Clone the repository**: `git clone [PLACEHOLDER]https://github.com/your-username/my-home-assistant.git`
 2. **Install dependencies**: Navigate to the project directory and run `pip install -r requirements.txt` to install necessary Python libraries.
-3. **API Keys**: Obtain necessary API keys (e.g., OpenWeatherMap API) and set them in the configuration file (`config.py`).
-4. **Run the Assistant**: Execute `python assistant.py` to start the home assistant. Ensure your microphone is set up correctly for voice commands.
+3. **API Keys**: Obtain necessary API keys (e.g., OpenWeatherMap API) and set them in the configuration file (`.env`) at the root level.
+4. **Run the Assistant**: Execute `main.py` to start the home assistant. Ensure your microphone is set up correctly for voice commands.
 
 ## Usage
 
--- main.py
+++ main.py
@@ -3,21 +3,35 @@ import threading
 import time
 from functionality.speech_recog import SpeechRecog
 import queue
+import copy
+
+# Import the actions
+from actions.weather import get_weather, get_weather_forecast
+from actions.find_location import get_location
+
+# Import the action router
+from functionality.action_router import get_action
+
+
 
 class App():
-    def __init__(self):
+    def __init__(self, command_dict: dict = {}):
         self.gui = tk.CTk()
-        self.gui.title("Custom Tkinter")
-        self.gui.geometry("300x200")
+        self.gui.title("Custom Home Voice Assistant")
+        self.gui.geometry("900x600")
         self.prompt_label = tk.CTkLabel(self.gui, text="Press the button and say something...", pady=35)
         self.prompt_label.pack()
         self.start_button = tk.CTkButton(self.gui, text="Start", command=self.start)
         self.start_button.pack()
+        self.status_label = tk.CTkLabel(self.gui, text="Status...", pady=35)
+        self.status_label.pack()
         self.output_label = tk.CTkLabel(self.gui, text="Readout here...", pady=35)
         self.output_label.pack()
         self.thread = None
-        
         self.state_queue = queue.Queue()
+        
+        self.trigger_phrase = "run command"
+        self.command_dict = command_dict
         
         self.speech_recog = SpeechRecog(self.state_queue)
 
@@ -30,8 +44,8 @@ class App():
     def check_queue(self):
         try:
             state = self.state_queue.get_nowait()  # Non-blocking check of the queue
-            self.output_label.configure(text=state)  # Update the label with the new state
-            print(f'GUI: Reflecting state change to {state}')
+            self.status_label.configure(text=state)  # Update the label with the new state
+            #print(f'GUI: Reflecting state change to {state}')
         except queue.Empty:
             pass  # No new state, do nothing
 
@@ -39,13 +53,45 @@ class App():
             self.gui.after(100, self.check_queue)
 
     def run_action(self):
-        self.output_label.configure(text=self.speech_recog.state)
+        self.status_label.configure(text=self.speech_recog.state)
         text = self.speech_recog.listen_for_speech()
-        self.thread = None
+        
+        
+        # Handle Printing the understood text to the gui
         time.sleep(0.11) # sleep longer than the next tick of the "check_queue" function
-        self.output_label.configure(text=text)
-
+        final_text = "You said: " + text
+        self.status_label.configure(text=final_text)
+        
+        # Handle running the command
+        if self.trigger_phrase in text:
+            user_command_req = text.split(self.trigger_phrase)[1].strip()
+            action_selected_response = get_action(user_command_req, self.command_dict)
+            
+            print(f"Action Selected Response: {action_selected_response}, type: {type(action_selected_response)}")
+            
+            if "path" in action_selected_response.keys():
+                key_nest_path = action_selected_response["path"]
+                running_dict = copy.deepcopy(self.command_dict)
+                # Convert the running_dict to a callable through digging down into the nested dictionary
+                try:
+                    for key in key_nest_path:
+                        running_dict = running_dict[key]
+                except KeyError as k:
+                    print(f"Invalid Action Path. Key Error: {k}")
+                    self.output_label.configure(text=f"Invalid Action Path. Key Error: {k}")
+                
+                # Run the callable
+                if callable(running_dict):
+                    print(f"Running Action: {running_dict.__name__}")
+                    action_return = running_dict()
+                    # render the return to the GUI
+                    self.output_label.configure(text=action_return)
+        
+        self.thread = None
+        return
 
 if __name__ == "__main__":
-    app = App()
+    commands = {"weather": {"Now":get_weather, "Forecast":get_weather_forecast}, "location": get_location}
+    
+    app = App(command_dict=commands)
     app.gui.mainloop()
